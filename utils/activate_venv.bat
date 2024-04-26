79809c900b11387e730d80f8e56c78ee037b0567
JohanEstebanCuervo <>
2024-04-26 18:23:45 -0500

    FIX: activate_venv code correction

--- utils/activate_venv.bat
+++ utils/activate_venv.bat
@@ -1,11 +1,2 @@
 cd ..
-@echo off
-if not exist "venv" (
-    virtualenv venv
-    call venv\Scripts\activate.bat
-    pip install -r admin_taxi_api\requirements.txt
-) else (
-    echo "Entorno virtual existente"
-)
-pause
-exit
+call venv\Scripts\activate.bat
