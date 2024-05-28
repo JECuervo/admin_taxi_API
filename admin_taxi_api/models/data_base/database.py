ff8c53a615d0a5e9f81cc9e4508db21fcdc2d4ee
JohanEstebanCuervo <>
2024-05-28 10:25:26 -0500

    ADD: activate foreign_keys=ON in database

--- admin_taxi_api/models/data_base/database.py
+++ admin_taxi_api/models/data_base/database.py
@@ -1,8 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./admin_taxi_api/models/data_base/admin_taxi_db.db"
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, _):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
