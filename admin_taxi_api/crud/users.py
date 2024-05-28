6c7d11649c5d1ee45378a300fb14251df98f7c3b
JohanEstebanCuervo <>
2024-05-28 10:30:58 -0500

    ADD: create_administrator function
    At this point the function does not yet verify the data entered, it only
    adds them to the database.

--- admin_taxi_api/crud/users.py
+++ admin_taxi_api/crud/users.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from ..models import models, schemas
 
 
-def create_user(db: Session, user: schemas.UsuarioCreate):
+def create_usuario(db: Session, user: schemas.UsuarioCreate) -> models.Usuario:
 
     db_user = models.Usuario(
         cel=user.cel,
@@ -13,3 +13,15 @@ def create_user(db: Session, user: schemas.UsuarioCreate):
     db.refresh(db_user)
 
     return db_user
+
+
+def create_administrador(
+    db: Session, admin: schemas.AdministradorCreate
+) -> models.Administrador:
+
+    db_admin = models.Administrador(id=admin.id, placa=admin.placa)
+    db.add(db_admin)
+    db.commit()
+    db.refresh(db_admin)
+
+    return db_admin
